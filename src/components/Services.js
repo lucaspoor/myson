7a9e379031b97e757ce36c0a0a1b2b8ce257a3d7
lucas <lucaspoor10@gmail.com>
2024-03-05 14:15:37 -0300

    xd

--- src/components/Services.js
+++ src/components/Services.js
@@ -17,17 +17,10 @@ export function Services() {
 
         <div className="row no-gutters">
           <div className="col-lg-4 col-md-6 col-sm-6">
-            <div className="text-center  px-4 py-5 hover-style-1">
-              <i className="icofont-gym-alt-2 text-lg text-color"></i>
-              <h4 className="mt-3 mb-4 text-uppercase">WEIGHT LIFTING</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et,
-                molestias.
-              </p>
-            </div>
+            <img></img>
           </div>
           <div className="col-lg-4 col-md-6 col-sm-6">
-            <div className="text-center  px-4 py-5 hover-style-1">
+            <div className="text-center  px-4 py-5 ">
               <i className="icofont-cycling-alt text-lg text-color"></i>
               <h4 className="mt-3 mb-4 text-uppercase">Cycling</h4>
               <p>
@@ -37,7 +30,7 @@ export function Services() {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 col-sm-6">
-            <div className="text-center  px-4 py-5 hover-style-1">
+            <div className="text-center  px-4 py-5 ">
               <i className="icofont-gym-alt-3 text-lg text-color"></i>
               <h4 className="mt-3 mb-4 text-uppercase">YOGA MEDIDATION</h4>
               <p>
@@ -47,7 +40,7 @@ export function Services() {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 col-sm-6">
-            <div className="text-center  px-4 py-5 hover-style-1">
+            <div className="text-center  px-4 py-5 ">
               <i className="icofont-muscle text-lg text-color"></i>
               <h4 className="mt-3 mb-4 text-uppercase">Building body</h4>
               <p>
@@ -57,7 +50,7 @@ export function Services() {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 col-sm-6">
-            <div className="text-center  px-4 py-5 hover-style-1">
+            <div className="text-center  px-4 py-5 ">
               <i className="icofont-dumbbell text-lg text-color"></i>
               <h4 className="mt-3 mb-4 text-uppercase">Fitness Track</h4>
               <p>
@@ -67,7 +60,7 @@ export function Services() {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 col-sm-6">
-            <div className="text-center  px-4 py-5 hover-style-1">
+            <div className="text-center  px-4 py-5 ">
               <i className="icofont-gym text-lg text-color"></i>
               <h4 className="mt-3 mb-4 text-uppercase">Fitness</h4>
               <p>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,3 +1,38 @@
+import projects from "../data/projects.json";
+
 export function Projects() {
-  return <></>;
+  return (
+    <>
+      <section className="mt-80px section services especific">
+        <div className="container ">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 text-center">
+              <div className="section-title">
+                <div className="divider mb-3"></div>
+                <h2>Our Services</h2>
+                <p>
+                  We offer more than 35 group exercis, aerobic classNamees each
+                  week.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="row ">
+              {projects.map((p) => {
+                return (
+                  <div className="col-lg-4 col-md-6">
+                    <div className="card  border-0 rounded-top border-bottom position-relative premium ">
+                      <img className="img-fluid" src={p.imagen}></img>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
